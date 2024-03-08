5bc314fbdd8f42170b95cad711de1e7952a98737
Ian Jennings <ian@meetjennings.com>
2024-03-08 08:20:59 -0600

    update

--- test.spec.js
+++ test.spec.js
@@ -6,42 +6,17 @@ expect.extend(lgtm);
 
 test("has title", async ({ page }) => {
   await page.goto("https://playwright.dev/");
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-
   await expect(page).lgtm("shows the playwright logo");
-});
-
-test("get started link", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-
-  // Click the get started link.
-  await page.getByRole("link", { name: "Get started" }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(
-    page.getByRole("heading", { name: "Installation" })
-  ).toBeVisible();
-
-  // for some rason this does not wait for page to be loaded
-
-  await expect(page).lgtm("shows the text 'introduction'");
 });
 
 test("github homepage", async ({ page }) => {
   await page.goto("https://github.com/");
-
   await expect(page).lgtm("6 brand logos show up");
 });
 
 test("ycombinator", async ({ page }) => {
   await page.goto("https://news.ycombinator.com");
-
-  await expect(page).lgtm("the header is blue");
-  await expect(page).lgtm("the header is orange");
-  await expect(page).lgtm("the header is not blue");
-  await expect(page).lgtm("the header is blue");
+  await expect(page).lgtm("there is an orange strip at the top of the page");
 });
 
 test("nytimes", async ({ page }) => {
@@ -49,4 +24,19 @@ test("nytimes", async ({ page }) => {
 
   await expect(page).lgtm("there is an ad on the homepage");
   await expect(page).lgtm("there is someting political on the homepage");
+});
+
+test("eloquent", async ({ page }) => {
+  await page.goto("https://eloquentjavascript.net/");
+  await expect(page).lgtm("there is bird on this page");
+});
+
+test("zombo", async ({ page }) => {
+  await page.goto("https://zombo.com/");
+  await expect(page).lgtm("page is colorful");
+});
+
+test("rickroll", async ({ page }) => {
+  await page.goto("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+  await expect(page).lgtm("video is not playing");
 });
