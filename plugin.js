61d8c44353e8f152a2defae400564eddff03f4a0
Ian Jennings <ian@meetjennings.com>
2024-03-08 08:49:39 -0600

    update server

--- plugin.js
+++ plugin.js
@@ -1,43 +1,60 @@
 import bufferToDataUrl from "buffer-to-data-url";
 const http = require("https");
 
-module.exports = {
-  goodlooks: async function (page, assertion) {
-    let screenshot = await page.screenshot({ encoding: "base64" });
+module.exports = function (key) {
+  if (!key) {
+    throw "Must supply api key. Get it at https://goodlooks.ai";
+  }
 
-    // const url = "http://localhost:3005/v1/goodlooks";
-    const url = "https://goodlooks-main-80a621c.d2.zuplo.dev/v1/goodlooks";
-    const options = {
-      method: "POST",
-      headers: {
-        Authorization: "Bearer zpka_05acaa405814447ba022449aa073fef5_78748307",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        screenshot: bufferToDataUrl("image/png", screenshot),
-        assertion,
-      }),
-    };
+  return {
+    goodlooks: async function (page, assertion) {
+      if (!assertion.length) {
+        throw "Must supply assertion";
+      }
 
-    let result = await fetch(url, options);
-    let json = await result.json();
+      if (!page) {
+        throw "Could not get page object";
+      }
 
-    if (json.indexOf("PASS") > -1) {
-      return {
-        pass: true,
-        message: () => `goodlooks.sh ${json}`,
-        name: "goodlooks",
-        expected: true,
-        actual: true,
+      let screenshot = await page.screenshot({ encoding: "base64" });
+
+      // const url = "http://localhost:3005/v1/goodlooks";
+      const url = "https://lgtm-main-80a621c.d2.zuplo.dev/v1/lgtm";
+      const options = {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${key}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          screenshot: bufferToDataUrl("image/png", screenshot),
+          assertion,
+        }),
       };
-    } else {
-      return {
-        pass: false,
-        message: () => `goodlooks.sh ${json}`,
-        name: "goodlooks",
-        expected: true,
-        actual: false,
-      };
-    }
-  },
+
+      let result = await fetch(url, options);
+
+      console.log(result.body());
+
+      let json = await result.json();
+
+      if (json.indexOf("PASS") > -1) {
+        return {
+          pass: true,
+          message: () => `goodlooks.sh ${json}`,
+          name: "goodlooks",
+          expected: true,
+          actual: true,
+        };
+      } else {
+        return {
+          pass: false,
+          message: () => `goodlooks.sh ${json}`,
+          name: "goodlooks",
+          expected: true,
+          actual: false,
+        };
+      }
+    },
+  };
 };
